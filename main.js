907a817fc1c1d2ebe10aec88b631a2d0a76e89cc
Emily <easchoettler@gmail.com>
2023-01-05 09:30:56 -0500

    Add solution to question 2

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ function sumRange(n) {
     }
     return n + sumRange(n-1);
 }
+
+// Question 2 - Power function
+
+function power(base, exp) {
+    if (exp === 0) return 1;
+    return base * power(base, exp - 1);
+}
