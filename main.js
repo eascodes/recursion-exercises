9e7741159b668374488449a480663df1ff78780c
Emily <easchoettler@gmail.com>
2023-01-05 09:59:02 -0500

    Add solution to question 3

--- main.js
+++ main.js
@@ -13,3 +13,10 @@ function power(base, exp) {
     if (exp === 0) return 1;
     return base * power(base, exp - 1);
 }
+
+// Question 3 - Calculate factorial
+
+function factorial(n) {
+    if (n === 1) return 1;
+    return n * factorial(n - 1);
+}
